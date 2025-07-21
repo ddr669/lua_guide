26bd3b24d850d34ad9eaf831ade05f2e749b323a
ddr669 <dudu.herique.x@gmail.com>
2025-07-20 21:28:57 -0300

    Hello, world in lua Pt-BR

---
+++ basics/hello_world.lua
@@ -0,0 +1,15 @@
+#!/bin/lua 
+
+--[[
+--	This is a large comment block
+--	very funny isnt?
+--
+--]]
+
+-- print olá mundo 
+
+print("Olá, Mundo!")
+print()
+
+io.write("Hello World, from ", _VERSION, "!\n")
+
